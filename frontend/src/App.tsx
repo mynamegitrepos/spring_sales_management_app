a6c84d7ab13da3f7ec257157c4cce3ca94dc808c
mynamegitrepos <samuelpereiradesouza665@gmail.com>
2022-07-11 16:17:08 -0300

    component progressing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotifiationButton from "./components/NotificationButton"
 
 function App() {
   return(
+    <>
     <h1>Teste</h1>
+    <NotifiationButton />
+    <NotifiationButton />
+    </>
   )
 }
 
