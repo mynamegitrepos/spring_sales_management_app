b61f5236f28ff8eb85c1c509139bb090f9528501
mynamegitrepos <samuelpereiradesouza665@gmail.com>
2022-07-18 00:50:26 -0300

    Toast

--- backend/src/main/java/com/dvsuperior/dsproject/services/SmsService.java
+++ backend/src/main/java/com/dvsuperior/dsproject/services/SmsService.java
@@ -34,8 +34,8 @@ public class SmsService {
 		Sale sale = saleRepository.findById(saleId).get();
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
-		String msg = "Vendedor " + sale.getSellerName() + "Foi destaque em " +
-		date + "com um total de R$ " + String.format("%.2f", sale.getAmount());
+		String msg = " Vendedor " + sale.getSellerName() + " Foi destaque em " +
+		date + " com um total de R$ " + String.format("%.2f", sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
